99b74a12135a3574617c6bcaad6a7dd7f2157390
Lucas Marcondes <lucas.github30@gmail.com>
2022-03-11 16:18:24 -0300

    Finalização do JS

---
+++ script/dadosDosprojetos.js
@@ -0,0 +1,20 @@
+const projetos = [
+    {
+        link: 'https://www.google.com',
+        img: './img/lucas-rm.github.io_Portfolio_.png',
+        titulo: 'Projeto 1',
+        descricao: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores explicabo, quidem amet nesciunt aliquid fugit maiores dolores earum? Enim dolores blanditiis qui possimus reiciendis dolorum cumque facere quos consequatur eveniet! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ducimus soluta doloremque ipsam nemo atque eum, repellat alias iure doloribus maiores dolorum voluptatum. Maxime hic quasi dolore sapiente ab voluptatum iure.'
+    },
+    {
+        link: '#',
+        img: './img/lucas-rm.github.io_Portfolio_.png',
+        titulo: 'Projeto 2',
+        descricao: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores explicabo, quidem amet nesciunt aliquid fugit maiores dolores earum? Enim dolores blanditiis qui possimus reiciendis dolorum cumque facere quos consequatur eveniet! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ducimus soluta doloremque ipsam nemo atque eum, repellat alias iure doloribus maiores dolorum voluptatum. Maxime hic quasi dolore sapiente ab voluptatum iure.Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores explicabo, quidem amet nesciunt aliquid fugit maiores dolores earum? Enim dolores blanditiis qui possimus reiciendis dolorum cumque facere quos consequatur eveniet! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ducimus soluta doloremque ipsam nemo atque eum, repellat alias iure doloribus maiores dolorum voluptatum. Maxime hic quasi dolore sapiente ab voluptatum iure.'
+    },
+    {
+        link: '#',
+        img: './img/lucas-rm.github.io_Portfolio_.png',
+        titulo: 'Projeto 3',
+        descricao: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores explicabo, quidem amet nesciunt aliquid fugit maiores dolores earum? Enim dolores blanditiis qui possimus reiciendis dolorum cumque facere quos consequatur eveniet! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ducimus soluta doloremque ipsam nemo atque eum, repellat alias iure doloribus maiores dolorum voluptatum. Maxime hic quasi dolore sapiente ab voluptatum iure.Ducimus soluta doloremque ipsam nemo atque eum, repellat alias iure doloribus maiores dolorum voluptatum. Maxime hic quasi dolore sapiente ab voluptatum iure.'
+    }
+];
